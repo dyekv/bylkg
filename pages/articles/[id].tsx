0dc74083e21732a1935da9695f7b5aec8bfc58f7
dyekv <kogaktt@gmail.com>
2020-12-16 00:16:36 +0900

    edit context type

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -1,5 +1,6 @@
 import {BlogData} from '../../utils/types'
 import {format} from 'date-fns'
+import {GetStaticProps} from 'next'
 
 type props = {
     blog:BlogData
@@ -33,13 +34,13 @@ export default function BlogId({ blog }:props) {
   };
   
   // データをテンプレートに受け渡す部分の処理を記述します
-  export const getStaticProps = async (context:any) => {
-      console.log('context >> ',context)
+  export const getStaticProps:GetStaticProps = async (context) => {
+    if(context.params === undefined){return {props:{blog:undefined}}}
     const id = context.params.id;
     const key = {
       headers: {'X-API-KEY': process.env.MICROCMS_API_KEY || ''},
     };
-    const data = await fetch(
+    const data:BlogData = await fetch(
       'https://bylkg.microcms.io/api/v1/articles/' + id,
       key,
     )
